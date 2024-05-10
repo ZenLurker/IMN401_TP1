e2bb69f80de62bda03721237f8ec741beb8c3556
ZenLurker <dylan.fournier@usherbrooke.ca>
2024-05-10 17:00:43 -0400

    vertex and fragment shaders for triangle
    Missing pipeline still?

--- TPIMN401-A/TPIMN401-A-main/TPIMN401/shaders/triangle-fs.glsl
+++ TPIMN401-A/TPIMN401-A-main/TPIMN401/shaders/triangle-fs.glsl
@@ -1,1 +1,9 @@
 #version 460
+
+out vec4 FragColor;
+
+uniform vec3 triangleColor;
+
+void main() {
+    FragColor = vec4(triangleColor, 1.0);
+}
--- TPIMN401-A/TPIMN401-A-main/TPIMN401/src/main.cpp
+++ TPIMN401-A/TPIMN401-A-main/TPIMN401/src/main.cpp
@@ -8,8 +8,6 @@
 #include "utils.hpp"
 
 namespace IMN401 {
-
-
 	void printProgramError(GLuint program)
 	{
 		GLuint isLinked;
@@ -30,8 +28,6 @@ namespace IMN401 {
 		}
 		else
 			std::cout << "Shader compilation : OK" << std::endl;
-
-
 	}
 
 	void printPipelineError(GLuint pipeline)
@@ -53,12 +49,21 @@ namespace IMN401 {
 		}
 		else
 			std::cout << "Pipeline : OK" << std::endl;
-
 	}
 
+	//Force dGPU usage on my AMD hybrid setup
+#ifdef _WIN32
+#ifdef __cplusplus
+	extern "C" {
+#endif
 
+		__declspec(dllexport) uint32_t NvOptimusEnablement = 1;
+		__declspec(dllexport) int  AmdPowerXpressRequestHighPerformance = 1;
 
-
+#ifdef __cplusplus
+	}
+#endif
+#endif
 
 
 	int main()
@@ -90,6 +95,10 @@ namespace IMN401 {
 			return EXIT_FAILURE;
 		}
 
+		// Initialization
+		// =====================
+		// TODO: init buffers, shaders, etc.
+		// cf. https://www.khronos.org/files/opengl46-quick-reference-card.pdf
 
 		// shaders
 		std::string strVS = readFile("shaders/triangle-vs.glsl");
@@ -97,21 +106,46 @@ namespace IMN401 {
 		std::string strFS = readFile("shaders/triangle-fs.glsl");
 		const GLchar* fsCode = strFS.c_str();
 
+		// Create shader program
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vsCode, nullptr);
+		glCompileShader(vertexShader);
 
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fsCode, nullptr);
+		glCompileShader(fragmentShader);
 
-		// Initialization
-		// =====================
-		// TODO: init buffers, shaders, etc.
-		// cf. https://www.khronos.org/files/opengl46-quick-reference-card.pdf
+		GLuint shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+		// Check for shader program linking errors
+		printProgramError(shaderProgram);
+
+		// Cleanup shaders
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		// Get the location of the uniform variable in the fragment shader
+		GLuint fragmentColorLocation = glGetUniformLocation(shaderProgram, "triangleColor");
+		if (fragmentColorLocation == -1) {
+			std::cerr << "Failed to get uniform location for triangleColor" << std::endl;
+		}
+
+		// Set the uniform color value
+		glUseProgram(shaderProgram);
+		GLfloat fragmentColor[] = { 1.0f, 0.0f, 0.0f }; // Red color
+		glUniform3fv(fragmentColorLocation, 1, fragmentColor);
 
 		glClearColor(0.3, 0.3, 0.3, 0);
-
 		glEnable(GL_DEPTH_TEST);
 
-		GLfloat vArray[]
-		{ -0.5f, -0.5f, 0.f,
-			0, 0.5f, 0.f,
-			0.5f, -0.5f, 0.f
+
+		glm::vec3 vArray[]
+		{ {-0.5f, -0.5f, 0.f},
+			{0, 0.5f, 0.f},
+			{0.5f, -0.5f, 0.f}
 		};
 
 		//Creation de VBO
@@ -129,13 +163,17 @@ namespace IMN401 {
 
 		//Activer chaque attribut de nos données
 		glEnableVertexArrayAttrib(VA, 0);
+		glEnableVertexArrayAttrib(VA, 1); //removing this doesnt break anything??
+
 		//Definir le formatage des VBOs
 		glVertexArrayAttribFormat(VA, 0, 3, GL_FLOAT, GL_FALSE, 0);
 
 		//Specifier le VBO a lire
 		glVertexArrayVertexBuffer(VA, 0, VBO, 0, sizeof(float) * 3);
 
+		//Atributes binding
 		glVertexArrayAttribBinding(VA, 0, 0);
+		glVertexArrayAttribBinding(VA, 1, 1);
 
 		if (glGetError() != GL_NO_ERROR) {
 			std::cerr << "OpenGL error" << std::endl;
@@ -150,8 +188,11 @@ namespace IMN401 {
 			glfwPollEvents();
 			// ==================
 			// TODO: render here !
-			glClear(GL_COLOR_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			//glBindProgramPipeline(pipeline);
 			glBindVertexArray(VA);
+
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 			// ==================
 
